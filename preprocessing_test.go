4caff7831079133248974ba19ff51f9c371f46c8
dereulenspiegel <till.klocke@gmail.com>
2025-01-27 20:18:06 +0100

    Fixing some of the test brittleness

--- preprocessing_test.go
+++ preprocessing_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/emersion/go-smtp"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
@@ -17,9 +18,14 @@ func TestSuccessfullPreProcessing(t *testing.T) {
 	rq, err := NewSQLiteWorkQueue[*ReceivedMessage](filepath.Join(t.TempDir(), "queue.db"), "send", 1, 90)
 	require.NoError(t, err)
 
+	timeout := time.NewTimer(time.Second * 5)
+	done := make(chan interface{})
+
 	sq := NewGenericWorkQueueMock[*QueuedMessage](t)
-	sq.On("Queue", mock.Anything, mock.MatchedBy(func(msg *QueuedMessage) bool {
-		return msg.From == "from@example.com"
+	sq.On("Queue", mock.Anything, mock.MatchedBy(func(msg *QueuedMessage) (ret bool) {
+		defer close(done)
+		ret = msg.From == "from@example.com" && msg.MailOpts.EnvelopeID == "foo-id" && msg.To == "to@example.com"
+		return
 	})).Once().Return(nil)
 
 	p, err := NewProcessorHandler(ctx, slog.Default(), rq, WithPreSendProcessors(SendProcessor(ctx, sq)))
@@ -34,12 +40,19 @@ func TestSuccessfullPreProcessing(t *testing.T) {
 			},
 		},
 		Body: []byte("foobar"),
+		MailOpts: &smtp.MailOptions{
+			EnvelopeID: "foo-id",
+		},
 	}
 
 	err = rq.Queue(ctx, rMsg)
 	require.NoError(t, err)
 
-	time.Sleep(time.Millisecond * 500)
+	select {
+	case <-timeout.C:
+		t.Fail()
+	case <-done:
+	}
 
 	sq.AssertExpectations(t)
 }
